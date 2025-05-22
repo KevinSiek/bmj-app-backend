6ff473ce31ba98848ef5630f9a02bc48249eec7a
LukasK <85026656+Zeon8985Army@users.noreply.github.com>
2025-05-22 14:33:44 +0700

    Return deleted code due to wrong merge and commit process

--- BmjAppBackend/app/Http/Controllers/EmployeeController.php
+++ BmjAppBackend/app/Http/Controllers/EmployeeController.php
@@ -163,6 +163,22 @@ class EmployeeController extends Controller
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
+    public function get(Request $request, $id)
+    {
+        try {
+            $employee = Employee::findOrFail($id);
+
+            return response()->json([
+                'message' => 'Employees retrieved successfully',
+                'data' => $employee
+            ], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Internal server error',
+                'error' => $th->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 
     public function getAll(Request $request)
     {
@@ -172,7 +188,8 @@ class EmployeeController extends Controller
 
             if ($q) {
                 $searchTerm = $q;
-                $query->where('fullname', 'like', "%$searchTerm%");
+                $query->where('fullname', 'like', "%$searchTerm%")
+                    ->orWhere('username', 'like', "%$searchTerm%");
             }
 
             $employees = $query->paginate(20);
@@ -230,30 +247,6 @@ class EmployeeController extends Controller
         } catch (\Throwable $th) {
             return response()->json([
                 'message' => 'Internal server error',
-                'error' => $th->getMessage()
-            ], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    // Retrieve an employee by their slug
-    public function get($slug)
-    {
-        try {
-            $employee = Employee::where('slug', '=', $slug)->first();
-
-            if (!$employee) {
-                return response()->json([
-                    'message' => 'Employee not found'
-                ], Response::HTTP_NOT_FOUND);
-            }
-
-            return response()->json([
-                'message' => 'Employee retrieved successfully',
-                'data' => $employee
-            ], Response::HTTP_OK);
-        } catch (\Throwable $th) {
-            return response()->json([
-                'message' => 'Failed to retrieve employee',
                 'error' => $th->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
--- BmjAppBackend/routes/api.php
+++ BmjAppBackend/routes/api.php
@@ -79,7 +79,7 @@ Route::middleware("auth:sanctum")->group(function () {
         // Employee Routes
         Route::prefix('employee')->group(function () {
             Route::get('/', [EmployeeController::class, 'getAll']);
-            Route::get('/{slug}', [EmployeeController::class, 'get']);
+            Route::get('/{id}', [EmployeeController::class, 'get']);
             Route::post('/', [EmployeeController::class, 'store']);
             Route::put('/{slug}', [EmployeeController::class, 'update']);
             Route::delete('/{slug}', [EmployeeController::class, 'destroy']);
