a777630ac74ebe682a3f5d13c24086e8af950e96
LukasK <85026656+Zeon8985Army@users.noreply.github.com>
2025-05-26 20:58:30 +0700

    Add data seeder for general table discount

--- BmjAppBackend/database/migrations/2024_12_29_150540_create_general.php
+++ BmjAppBackend/database/migrations/2024_12_29_150540_create_general.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('general', function (Blueprint $table) {
+        Schema::create('generals', function (Blueprint $table) {
             $table->id();
             $table->decimal('discount');
             $table->timestamps();
--- BmjAppBackend/database/seeders/DatabaseSeeder.php
+++ BmjAppBackend/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            GeneralSeeder::class,
             CustomerSeeder::class,
             AccessRoleSeeder::class,
             SparepartSeeder::class,
@@ -26,7 +27,6 @@ class DatabaseSeeder extends Seeder
             BackOrderSeeder::class,
             BuySeeder::class,
             EmployeeSeeder::class
-    ]);
+        ]);
     }
 }
-
