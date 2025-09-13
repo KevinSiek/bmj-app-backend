8cdbb16c9278fea2a485d9244a53d4b512d7fde4
Kevin Widjanarko <kevin.widjanarko@gdn-commerce.com>
2025-09-13 12:41:29 +0700

    refactor

--- BmjAppBackend/app/Http/Controllers/QuotationController.php
+++ BmjAppBackend/app/Http/Controllers/QuotationController.php
@@ -861,7 +861,7 @@ class QuotationController extends Controller
             $request->validate([
                 'notes' => 'required|string',
             ]);
-            $note = $request->input('notes');
+            $notes = $request->input('notes');
 
             $quotations = $this->getAccessedQuotation($request);
             $quotation = $quotations->where('slug', $slug)->first();
@@ -936,7 +936,7 @@ class QuotationController extends Controller
                 'purchase_order_number' => $purchaseOrderNumber,
                 'purchase_order_date' => now(),
                 'employee_id' => $quotation->employee_id,
-                'notes' => $note,
+                'notes' => $notes,
                 'current_status' => PurchaseOrderController::PREPARE,
                 'version' => 1
             ]);
